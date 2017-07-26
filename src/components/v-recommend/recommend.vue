e587110561b212bf55b0cad6ce1e004b8e8667d2
Tetegw <1151832942@qq.com>
2017-07-26 21:35:08 +0800

    [Added]  slider done

--- src/components/v-recommend/recommend.vue
+++ src/components/v-recommend/recommend.vue
@@ -1,13 +1,94 @@
 <template>
     <div class="v-recommend">
-        推荐
+        <div class="recommend-content">
+            <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+                <v-slider>
+                    <div v-for="item in recommends" :key="item.id">
+                        <a :href="item.linkUrl">
+                            <img  :src="item.picUrl">
+                        </a>
+                    </div>
+                </v-slider>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    
+import Slider from '@/components/b-slider/slider'
+import {getRecommend} from '@/api/recommend'
+import {ERR_OK} from '@/api/config' 
+export default {
+    data(){
+        return{
+            recommends: [],
+        }
+    },
+    components:{
+        'v-slider': Slider,
+    },
+    created(){
+        this._getRecommend()
+    },
+    methods:{
+        _getRecommend() {
+            getRecommend().then((res) => {
+                if (res.code === ERR_OK) {
+                    this.recommends = res.data.slider
+                }
+            })
+        },
+    }
+}
 </script>
 
-<style lang="stylus">
-    
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+    .v-recommend
+        position: fixed
+        width: 100%
+        top: 88px
+        bottom: 0
+        .recommend-content
+            height: 100%
+            overflow: hidden
+            .slider-wrapper
+                position: relative
+                width: 100%
+                overflow: hidden
+            .recommend-list
+                .list-title
+                    height: 65px
+                    line-height: 65px
+                    text-align: center
+                    font-size: $font-size-medium
+                    color: $color-theme
+                .item
+                    display: flex
+                    box-sizing: border-box
+                    align-items: center
+                    padding: 0 20px 20px 20px
+                    .icon
+                        flex: 0 0 60px
+                        width: 60px
+                        padding-right: 20px
+                    .text
+                        display: flex
+                        flex-direction: column
+                        justify-content: center
+                        flex: 1
+                        line-height: 20px
+                        overflow: hidden
+                        font-size: $font-size-medium
+                        .name
+                            margin-bottom: 10px
+                            color: $color-text
+                        .desc
+                            color: $color-text-d
+            .loading-container
+                position: absolute
+                width: 100%
+                top: 50%
+                transform: translateY(-50%)
 </style>
