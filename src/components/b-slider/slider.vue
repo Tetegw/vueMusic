ea32d483dd33a8dc3ec426e06d669d60e7201bce
Tetegw <1151832942@qq.com>
2017-07-26 22:15:35 +0800

    [fix] slider click BUG done

--- src/components/b-slider/slider.vue
+++ src/components/b-slider/slider.vue
@@ -5,7 +5,7 @@
             </slot>
         </div>
         <div class="dots">
-            <span class="dot"  v-for="(item, index) in dots" :class="{"active": currentPageIndex === index }"></span>
+            <span class="dot"  v-for="(item, index) in dots" :class="{'active': currentPageIndex === index }"></span>
         </div>
     </div>
 </template>
@@ -34,14 +34,28 @@ export default{
         }
     },
     mounted() {
-      setTimeout(() => {
-        this._setSliderWidth()
-        this._initDots()
-        this._initSlider()
-      }, 20)
+        setTimeout(() => {
+            this._setSliderWidth()
+            this._initDots()
+            this._initSlider()
+
+            if (this.autoPlay) {
+                this._play()
+            }
+        }, 20)
+
+        window.addEventListener('resize', () => {
+            if(!this.slider){
+                return
+            }
+            this._setSliderWidth(true)
+            this.slider.refresh()
+        })
+
+
     },
     methods:{
-        _setSliderWidth(){
+        _setSliderWidth(isResize){
             this.children = this.$refs.sliderGroup.children
 
             let width = 0
@@ -52,7 +66,7 @@ export default{
                 child.style.width = sliderWidth + 'px'
                 width += sliderWidth
             }
-            if (this.loop) {
+            if (this.loop && !isResize) {
                 width += 2 * sliderWidth
             }
             this.$refs.sliderGroup.style.width  = width + 'px'
@@ -65,7 +79,7 @@ export default{
                 snap: true,
                 snapLoop: this.loop,
                 snapThreshold: 0.3,
-                snapSpeed: 400
+                snapSpeed: 400,
             })
 
             this.slider.on('scrollEnd', () => {
@@ -74,11 +88,25 @@ export default{
                     pageIndex -= 1
                 }
                 this.currentPageIndex = pageIndex
+
+                if(this.autoPlay){
+                    clearTimeout(this.timer)
+                    this._play()
+                }
             })
         },
         _initDots() {
             this.dots = new Array(this.children.length)
         },
+        _play() {
+            let pageIndex = this.currentPageIndex + 1
+            if (this.loop) {
+                pageIndex += 1
+            }
+            this.timer = setTimeout(() => {
+                this.slider.goToPage(pageIndex, 0, 400)
+            }, this.interval)
+        }
     }
 }
 </script>
